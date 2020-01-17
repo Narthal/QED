95b33dce38a3016aabdfde5b17d193abdcdf5d52
Szoke Balazs <bala.szoke@gmail.com>
2020-01-17 23:15:16 +0100

    update automation

--- Automation/Scripts/rebuildGLFW.bat
+++ Automation/Scripts/rebuildGLFW.bat
@@ -1,9 +1,10 @@
 cd ../../Build
 mkdir GLFW
-cmake ../External\Dependencies\GLFW\
+cd ../
+External\Tools\cmake-3.16.2-win64-x64\bin\cmake.exe -S External\Dependencies\GLFW\ -B Build\GLFW\
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-cd ../External\Dependencies\GLFW\src
+cd Build\GLFW\src
 msbuild glfw.vcxproj /p:OutDir=C:\Users\balas\source\GitHubRepos\QED\Build\GLFW\ /p:IntDir=C:\Users\balas\source\GitHubRepos\QED\Build\GLFW\Intermediate\ /p:Configuration=Release /p:Platform=x64
 
 pause
