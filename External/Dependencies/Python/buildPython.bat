04db25d0368a88f6728557b324baf0154bc59ad8
Szoke Balazs <bala.szoke@gmail.com>
2020-05-01 19:52:23 +0200

    Python : build script for windows

---
+++ External/Dependencies/Python/buildPython.bat
@@ -0,0 +1,2 @@
+cd ../../Vendor/cpython/PCbuild
+build.bat -M "/p:OutDir=C:\Users\balas\source\GitHubRepos\QED\Build\Python" "/p:IntDir=C:\Users\balas\source\GitHubRepos\QED\Build\Python/Intermediate/"
