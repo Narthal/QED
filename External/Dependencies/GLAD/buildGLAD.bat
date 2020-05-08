2bb3841891fa4592111b442a16f60b38774a4484
Szoke Balazs <bala.szoke@gmail.com>
2020-05-08 13:20:49 +0200

    GLAD : codeGen batch file fixup

--- External/Dependencies/GLAD/buildGLAD.bat
+++ External/Dependencies/GLAD/buildGLAD.bat
@@ -1,2 +1,3 @@
+set PYTHONHOME=C:\Users\balas\source\GitHubRepos\QED\External\Vendor\cpython
 cd ../../Vendor/GLAD
 C:\Users\balas\source\GitHubRepos\QED\Build\Python\Python.exe -m glad --profile=core --api="gl=4.5" --generator=c --out-path=../../Dependencies\GLAD\codeGen --reproducible
